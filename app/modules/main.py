47824436f611012d0e83f53c3f26a8e3bbf4e84a
yf <420866750@qq.com>
2016-08-14 21:12:14 +0800

    basic cms

--- app/modules/main.py
+++ app/modules/main.py
@@ -13,60 +13,9 @@ class MainHandler(main):
     def get(self):
         self.current_user = self.get_current_user()
         if self.current_user:
-            self.render('object.new.html', hl='new-object')
+            self.render('sys.html', hl='main')
         else:
             self.redirect('/login')
-
-class ObjectAddHandler(main):
-    '''
-    yf: 编辑专题
-    '''
-    def get(self):
-        pass
-
-    def post(self):
-        import os
-        dat = self.json_decode(self.request.body)
-        if 'id' not in dat: # 添加
-            oid = self.db.object.add(**dat)
-            if oid: # 添加模板
-                ph = os.path.dirname(__file__).split("app")[0] + 'templates\\s\\'+str(oid)
-                os.makedirs(ph)
-                open("./templates/s/"+str(oid)+"/index.html", "w")
-        else:
-            pass
-
-class ObjectsHandler(main):
-    '''
-    yf: 专题
-    '''
-    def get(self):
-        objs = self.db.object().data
-        self.render('object.list.html', hl='list-object', objs=objs)
-
-class CateHandler(main):
-    '''
-    yf: 分类
-    '''
-    def get(self):
-        cates = self.db.category().data
-        self.render('cate.list.html', hl='list-cate', cates=cates)
-
-class CateEditHandler(main):
-    '''
-    yf: 编辑分类
-    '''
-    def get(self):
-        self.render('cate.edit.html', hl='edit-cate')
-
-    def post(self):
-        dat = self.json_decode(self.request.body)
-        if 'id' in dat: # 编辑
-            pass
-        else: # 添加
-            cid = self.db.category.add(**dat)
-            if cid:
-                self.redirect('/cate')
 
 class LoginHandler(main):
     '''
@@ -99,10 +48,6 @@ url_prefix = ''
 
 urls = [
     ('/', MainHandler),
-    ('/objlist', ObjectsHandler),
-    ('/objadd', ObjectAddHandler),
-    ('/cate', CateHandler),
-    ('/editcate', CateEditHandler),
     ('/login', LoginHandler),
     ('/s/(\d+)/', PagesHandler)
 ]
